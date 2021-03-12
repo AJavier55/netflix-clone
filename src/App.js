219e81da892cad7556bb9c814457c4892ca537a2
AJavier55 <aliciajavier5@gmail.com>
2021-03-11 19:04:59 -0500

    got images working.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react"
 import './App.css';
 import requests from "./requests";
-import Row from "./Row"
+import Row from "./Row";
 
 
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Netflix Originals"/>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       
     </div>
   );
